f6bd1a6015f7d064529aeda7e326d25c6eb2a692
VashuSharma14 <95088468+VashuSharma14@users.noreply.github.com>
2022-09-13 00:25:11 +0530

    Update p6.java

--- p6.java
+++ p6.java
@@ -9,5 +9,6 @@ public static void main(String args[]){
     System.out.println(s.equalsIgnoreCase("JAVA"));                              //(vi)
     System.out.println("last occurence index of a="+s.lastIndexOf('a'));           //(vii)
     }
+}
 
 
